e8f6ab347838c8948e313f0ec415b32725f4734d
satiseven <satiseven777@gmail.com>
2021-11-02 10:49:11 +0300

    remove store

--- store/reducers/index.ts
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, combineReducers } from "redux";
-import thunk from "redux-thunk";
-import { AppState, todoIn } from "../../types/actions";
-import todoReducer from "./todo";
-
-const rootReducer = combineReducers<AppState>({
-  todo: todoReducer,
-});
-export default rootReducer;
