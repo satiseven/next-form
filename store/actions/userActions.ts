23e4da89e831ff6e8f75d26eae088f146cde3ba3
satiseven <satiseven777@gmail.com>
2021-11-27 13:29:37 +0300

    get users list

---
+++ store/actions/userActions.ts
@@ -0,0 +1,11 @@
+import { Dispatch } from "react";
+import { apiUrl } from "../../config/api";
+import { usersList, SET_USERS_LIST } from "../../types/actions";
+
+export function setUsersList() {
+  return function (dispatch: Dispatch<usersList>) {
+    fetch(`${apiUrl}/users`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: SET_USERS_LIST, payload: data }));
+  };
+}
--- types/actions/userActions.ts
+++ types/actions/userActions.ts
@@ -1,9 +1,15 @@
+import { userInterface } from "../models/user";
 export const USER_GET = "USER_GET";
-export const USERS_LIST = "USERS_LIST";
-export type usersList = () => {
-  type: typeof USERS_LIST;
+export const GET_USERS_LIST = "GET_USERS_LIST";
+export const SET_USERS_LIST = "SET_USERS_LIST";
+export type usersList = {
+  type: typeof SET_USERS_LIST;
+  payload: userInterface[];
 };
-export type userGet = () => {
+export type userGet = {
   type: typeof USER_GET;
+};
+export type usersListGet = {
+  type: typeof GET_USERS_LIST;
 };
 export type userAction = usersList | userGet;
